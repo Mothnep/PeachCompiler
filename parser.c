ea64b968b7826377ee16296c3b3f444c8937eb4b
Mothnep <thomas.penner.pro@gmail.com>
2024-08-21 18:48:04 +0200

    Datatypes and keywords 1

--- parser.c
+++ parser.c
@@ -99,7 +99,7 @@ static int parser_get_precedence_for_operator(const char *op, struct expressiona
     }
     return -1; // can't find precedence
 }
-static bool parser_left_op_has_priority(const char *op_left, const char *op_right) //checks if left operator has priority
+static bool parser_left_op_has_priority(const char *op_left, const char *op_right) // checks if left operator has priority
 {
     struct expressionable_op_precedence_group *group_left = NULL;
     struct expressionable_op_precedence_group *group_right = NULL;
@@ -110,21 +110,23 @@ static bool parser_left_op_has_priority(const char *op_left, const char *op_righ
     int precedence_left = parser_get_precedence_for_operator(op_left, &group_left);
     int precedence_right = parser_get_precedence_for_operator(op_right, &group_right);
 
-    if(group_left->associtivity ==ASSOCIATIVITY_RIGHT_TO_LEFT){//can't handle this here
+    if (group_left->associtivity == ASSOCIATIVITY_RIGHT_TO_LEFT)
+    { // can't handle this here
         return false;
     }
     return precedence_left <= precedence_right;
 }
 
-void parser_node_shift_children_left(struct node* node){
+void parser_node_shift_children_left(struct node *node)
+{
     assert(node->type == NODE_TYPE_EXPRESSION);
     assert(node->exp.right->type == NODE_TYPE_EXPRESSION);
-    const char* right_op = node->exp.right->exp.op;
-    struct node* new_exp_left_node = node->exp.left;
-    struct node* new_exp_right_node = node->exp.right->exp.left;
+    const char *right_op = node->exp.right->exp.op;
+    struct node *new_exp_left_node = node->exp.left;
+    struct node *new_exp_right_node = node->exp.right->exp.left;
     make_exp_node(new_exp_left_node, new_exp_right_node, node->exp.op);
-    struct node* new_left_operand = node_pop();
-    struct node* new_right_operand = node->exp.right->exp.right;
+    struct node *new_left_operand = node_pop();
+    struct node *new_right_operand = node->exp.right->exp.right;
     node->exp.left = new_left_operand;
     node->exp.right = new_right_operand;
     node->exp.op = right_op;
@@ -142,20 +144,21 @@ void parser_reorder_expression(struct node **node_out)
         return;
     }
     // Possible expression if right operand is expression like this 50*E(30+20)
-/*
-* 50*E(30+20)
-* 50*EXPRESSION                     Watch video30 at around 14:00 on TL
-* EXPRESSION(50*EXPRESSION) -> "root expression"
-* code below is going to try and do (50*30)+20
-*
-*/
+    /*
+     * 50*E(30+20)
+     * 50*EXPRESSION                     Watch video30 at around 14:00 on TL
+     * EXPRESSION(50*EXPRESSION) -> "root expression"
+     * code below is going to try and do (50*30)+20
+     *
+     */
 
     if (node->exp.left->type != NODE_TYPE_EXPRESSION && node->exp.right && node->exp.right->type == NODE_TYPE_EXPRESSION)
     {
         const char *right_op = node->exp.right->exp.op;
-        if(parser_left_op_has_priority(node->exp.op, right_op)){ /* 50 in example above is not an expression,
-        so grabbing the '*' with node->exp.op and the '+' will be putt in rightt_op */
-            
+        if (parser_left_op_has_priority(node->exp.op, right_op))
+        { /* 50 in example above is not an expression,
+so grabbing the '*' with node->exp.op and the '+' will be putt in rightt_op */
+
             parser_node_shift_children_left(node);
 
             parser_reorder_expression(&node->exp.left);
@@ -195,10 +198,78 @@ int parse_exp(struct history *history)
     parse_exp_normal(history);
     return 0;
 }
-void parse_identifier(struct history* history){
+void parse_identifier(struct history *history)
+{
     assert(token_peek_next()->type == NODE_TYPE_IDENTIFIER);
     parse_single_token_to_node();
+}
 
+static bool is_keyword_variable_modifier(const char *val)
+{
+    return S_EQ(val, "unsigned") ||
+           S_EQ(val, "signed") ||
+           S_EQ(val, "static") ||
+           S_EQ(val, "const") ||
+           S_EQ(val, "extern") ||
+           S_EQ(val, "__ignore_typecheck__");
+}
+void parse_datatype_modifiers(struct datatype* dtype){
+    struct token* token = token_peek_next();
+    while(token && token->type == TOKEN_TYPE_KEYWORD){
+        if(!is_keyword_variable_modifier(token->sval)){
+            break;
+        }
+        if (S_EQ(token->sval, "signed"))
+        {
+            dtype->flags |= DATATYPE_FLAG_IS_SIGNED;
+        }
+        else if(S_EQ(token->sval, "unsigned"))
+        {
+            dtype->flags &= ~DATATYPE_FLAG_IS_SIGNED;
+        }
+        else if(S_EQ(token->sval, "static"))
+        {
+            dtype->flags |= DATATYPE_FLAG_IS_STATIC;
+        }
+        else if(S_EQ(token->sval, "const"))
+        {
+            dtype->flags |= DATATYPE_FLAG_IS_CONST;
+        }
+        else if(S_EQ(token->sval, "extern"))
+        {
+            dtype->flags |= DATATYPE_FLAG_IS_EXTERN;
+        }
+        else if(S_EQ(token->sval, "__ignore_typecheck__"))
+        {
+            dtype->flags |= DATATYPE_FLAG_IGNORE_TYPE_CHECKING;
+        }
+
+        token_next();
+        token = token_peek_next();
+    }
+}
+
+void parse_datatype(struct datatype *dtype)
+{
+    memset(dtype, 0, sizeof(struct datatype));
+    dtype->flags |= DATATYPE_FLAG_IS_SIGNED;
+    parse_datatype_modifiers(dtype);
+    parse_datatype_type(dtype);
+    parse_datatype_modifiers(dtype);
+}
+void parse_variable_function_or_struct_union(struct history *history)
+{
+    struct datatype dtype;
+    parse_datatype(&dtype);
+}
+void parse_keywords(struct history *history)
+{
+    struct token *token = token_peek_next();
+    if (is_keyword_variable_modifier(token->sval) || keyword_is_datatype(token->sval))
+    {
+        parse_variable_function_or_struct_union(history);
+        return;
+    }
 }
 
 int parse_expressionable_single(struct history *history)
@@ -216,13 +287,18 @@ int parse_expressionable_single(struct history *history)
         parse_single_token_to_node();
         res = 0; // succesfull operation
         break;
-    
+
     case TOKEN_TYPE_IDENTIFIER:
         parse_identifier(history);
         break;
 
     case TOKEN_TYPE_OPERATOR:
         parse_exp(history);
+        res = 0;
+        break;
+
+    case TOKEN_TYPE_KEYWORD:
+        parse_keywords(history);
         res = 0;
         break;
     }
